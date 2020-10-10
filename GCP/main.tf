fa75f5462a8fa9896ab80576658bf10bd134d2ed
Son Nguyen <firefumavn@gmail.com>
2020-10-11 00:53:10 +0700

    Add DNS

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,11 +1,11 @@
 #VPC
 module "vpc" {
-  source      = "./modules/vpc"
-  #vpc_id     = module.vpc.vpc_id
-  env         = var.env
-  region      = var.region
-  zone        = var.zone
-  project	    = var.project
+  source       = "./modules/vpc"
+  #vpc_id      = module.vpc.vpc_id
+  env          = var.env
+  region       = var.region
+  zone         = var.zone
+  project	   = var.project
 }
 
 #GCE
@@ -14,20 +14,21 @@ module "gce" {
   env         = var.env
   region      = var.region
   zone        = var.zone
-  project	    = var.project
+  project	  = var.project
   network     = module.vpc.vpc_id
   subnetwork  = module.vpc.subnetwork_id 
   address_ip  = module.vpc.address_ip
 }
 
-#Cloud DB
-module "db" {
-  source      = "./modules/clouddb"
+#DNS
+module "dns" {
+  source      = "./modules/dns"
   env         = var.env
   region      = var.region
   zone        = var.zone
-  project	    = var.project
+  project	  = var.project
   network     = module.vpc.vpc_id
-  network_link  = module.vpc.vpc_link 
+  subnetwork  = module.vpc.subnetwork_id 
   address_ip  = module.vpc.address_ip
+  lb_ip       = "0.0.0.0"
 }
--- GCP/provider.tf
+++ GCP/provider.tf
@@ -3,6 +3,7 @@ provider "google" {
   project     = var.project
   region      = var.region
   version     = "~> 3.0"
+  
 }
 
 provider "google-beta" {
